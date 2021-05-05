6f40e7bfea7b052c494f3cd7b7fcc9d1ec1edbf9
esben holk nielsen <esbenholknielsen@gmail.com>
2021-05-05 22:13:05 +0200

    removed all components for deployment test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+/* eslint-disable no-lone-blocks */
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./components/Home.js";
-import About from "./components/About.js";
-import SinglePost from "./components/SinglePost.js";
-import Posts from "./components/Posts.js";
 import NavBar from "./components/NavBar.js";
-import Audio from "./components/Audio.js";
-import Worldbuilder from "./components/Worldbuilder";
-import Skeleton from "./components/Skeleton";
-import Projects from "./components/Projects";
 
+import Home from "./components/Home.js";
+// import About from "./components/About.js";
+// import SinglePost from "./components/SinglePost.js";
+// import Posts from "./components/Posts.js";
+// import Audio from "./components/Audio.js";
+// import Worldbuilder from "./components/Worldbuilder";
+// import Skeleton from "./components/Skeleton";
+// import Projects from "./components/Projects";
+{
+  /* <Route path="/about">
+<About />
+</Route>
+<Route path="/projects/:slug">
+<SinglePost />
+</Route>
+<Route path="/posts">
+<Posts />
+</Route>
+<Route path="/projects">
+<Projects />
+</Route>
+<Route path="/audio-visualiser">
+<Audio />
+</Route>
+<Route path="/worldbuilder">
+<Worldbuilder />
+</Route>
+<Route path="/skeleton">
+<Skeleton />
+</Route> */
+}
 import "./App.css";
 
 function App() {
@@ -22,27 +46,6 @@ function App() {
           <Switch>
             <Route exact path="/">
               <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/projects/:slug">
-              <SinglePost />
-            </Route>
-            <Route path="/posts">
-              <Posts />
-            </Route>
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/audio-visualiser">
-              <Audio />
-            </Route>
-            <Route path="/worldbuilder">
-              <Worldbuilder />
-            </Route>
-            <Route path="/skeleton">
-              <Skeleton />
             </Route>
           </Switch>
         </main>
