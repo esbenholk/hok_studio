e064ed7182b25a6ebcd66bea8d9d5e573860bd92
esben holk nielsen <esbenholknielsen@gmail.com>
2021-05-06 16:21:31 +0200

    auth key removed from sanityClient

--- src/client.js
+++ src/client.js
@@ -4,6 +4,5 @@ export default sanityClient({
   projectId: "swdt1dj3",
   dataset: "production",
   apiVersion: "2021-03-25", // use a UTC date string
-  token: "", // or leave blank for unauthenticated usage
   useCdn: true, // `false` if you want to ensure fresh data
 });
